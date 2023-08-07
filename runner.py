41319d708002ac89e1dcbf57c3a8f02de34139bc
Plămădeală Max <77546227+grumpycatyo-collab@users.noreply.github.com>
2023-08-07 20:54:45 +0300

    changed tt, added runner_tracking

--- runner.py
+++ runner.py
@@ -8,8 +8,8 @@ from sort import *
 # cap.set(3,1280)
 # cap.set(4,720)
 
-cap = cv2.VideoCapture("data/los_angeles.mp4")
-model =  YOLO("yolos/yolov8n.pt")
+cap = cv2.VideoCapture("data/power_rangers.mp4")
+model =  YOLO("yolos/yolov8l.pt")
 
 classes = {0: 'person',
  1: 'bicycle',
@@ -93,18 +93,14 @@ classes = {0: 'person',
  79: 'toothbrush'}
 
 result_array = [classes[i] for i in range(len(classes))]
-mask = cv2.imread("r.jpg")
 
+mask = cv2.imread("r2.jpg")
 
-#tracker = DeepSort(max_age=50)
 tracker = Sort(max_age=20, min_hits=2,iou_threshold=0.3)
-l = [593,500,958,500]
-totalCount = []
-
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 size = (frame_width, frame_height)
-m = cv2.VideoWriter('traffic_tracking.avi', 
+m = cv2.VideoWriter('runner_tracking.avi', 
                          cv2.VideoWriter_fourcc(*'MJPG'),
                          10, size)
 size = (frame_width, frame_height)
@@ -120,26 +116,22 @@ while True:
             x1,y1, x2, y2 = box.xyxy[0]
             x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
             w,h = x2-x1,y2-y1
-            cvzone.cornerRect(frame,(x1,y1,w,h),l=5, rt = 2, colorC=(255,215,0), colorR=(255,99,71))
-            conf = math.ceil((box.conf[0]*100))/100
+            
             cls = int(box.cls[0])
+            if result_array[cls] =='person':
+                cvzone.cornerRect(frame,(x1,y1,w,h),l=5, rt = 2, colorC=(0,0,255), colorR=(71,99,255))
+            conf = math.ceil((box.conf[0]*100))/100
             ins = np.array([x1,y1,x2,y2,conf])
             detections = np.vstack((detections,ins))
 
     
     tracks = tracker.update(detections)
-    cv2.line(frame, (l[0],l[1]),(l[2],l[3]),color=(255,0,0),thickness=3)
     for result in tracks:
         x1,y1,x2,y2,id = result
         x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
         print(result)
-        cvzone.putTextRect(frame,f'{result_array[cls]} {conf} id:{int(id)} ',(max(0,x1),max(35,y1-20)),scale=1, thickness=1, offset=3, colorR=(255,99,71))
-        cx,cy = x1+w//2,y1+h//2
-        if l[0]<cx<l[2] and l[1]-10<cy<l[3]+10:
-            if totalCount.count(id) == 0:
-                totalCount.append(id)
-                cv2.line(frame, (l[0],l[1]),(l[2],l[3]),color=(127,255,212),thickness=5)
-    cvzone.putTextRect(frame,f' Total Count: {len(totalCount)} ',(70,70),scale=2, thickness=1, offset=3, colorR=(255,99,71))
+        if result_array[cls] =='person':
+            cvzone.putTextRect(frame,f'{result_array[cls]} {conf} id:{int(id)} ',(max(0,x1),max(35,y1-20)),scale=1, thickness=1, offset=3, colorR=(71,99,255))
     m.write(frame)
     cv2.imshow("Image",frame)
     cv2.waitKey(1)
